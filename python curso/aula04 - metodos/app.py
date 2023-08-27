2d9753432c1e6e3a8bf2ee6da38b0acf6eeb3572
Lucas Machado <111795994+lucasmsdev@users.noreply.github.com>
2023-08-27 15:46:34 -0300

    Add files via upload

---
+++ python curso/aula04 - metodos/app.py
@@ -0,0 +1,17 @@
+def soma_dois_valores(v1,v2):
+    return v1+v2
+# Este def foi um metodo que criei
+
+result = soma_dois_valores(3,4)
+print (result)
+
+# Built in - Imbutidas
+# print = imprime na tela
+# int, float = muda numbers
+# len = ve quantas letras tem uma string
+# abs = verifica o valor absoluto
+# lista = uma lista
+# sum = obten a a soma da 
+# min = verifica o valor minimo 
+# max = verifica o valor max
+# round = arredonda um numbero, [e possivel arredondar para mais de uma casa decimal]
